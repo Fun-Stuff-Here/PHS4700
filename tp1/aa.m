a6cc958714157510f7b8985c2d93ff6e4d794663
Nicolas Depelteau <nicolas@depelteau.ca>
2022-09-14 12:15:43 -0400

    fix typo

--- tp1/aa.m
+++ tp1/aa.m
@@ -2,7 +2,7 @@ function aaOut = aa(context)
 
     % Calcul en referenciel du drone
     % Force en jeu, poid, moteur
-    ForceMoteur_CM = [0; 0; sum(Lambda * context.F_max)]
+    ForceMoteur_CM = [0; 0; sum(context.Lambda * context.F_max)]
 
 %    TorqueMoteur_CM = 
 
