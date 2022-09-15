3fdc0928d3b54adeb6bd058facfa508d11716aee
Maxime Laroche <mlaroche2009@gmail.com>
2022-09-14 21:19:58 -0400

    Calcul CDM demi sphere

--- tp1/pcm.m
+++ tp1/pcm.m
@@ -1,3 +1,12 @@
 function pcmOut = pcm(context)
-    pcmOut = zeros(3,1);
+    drone = {};
+    drone.pos = context.pos;
+    drone.masse = context.m_s;
+    drone.rayon = context.R_s;
+    drone.volume = (1/2) * (4/3) * pi * drone.rayon ^ 3;
+    drone.masse_volumique = drone.masse / drone.volume;
+    z_cdm = pi * drone.masse_volumique * drone.rayon ^ 4 / (8*drone.masse);
+    drone.CDM = [0; 0; z_cdm];
+
+    pcmOut = drone.CDM; % Ajuster avec le CDM des bras/moteurs et du Colis
 endfunction
