605b5a7616a20ded20089a39d4534302f6a64a2d
Maxime Laroche <mlaroche2009@gmail.com>
2022-09-14 21:48:11 -0400

    corrected typo in z_cdm

--- tp1/pcm.m
+++ tp1/pcm.m
@@ -5,7 +5,7 @@ function pcmOut = pcm(context)
     drone.rayon = context.R_s;
     drone.volume = (1/2) * (4/3) * pi * drone.rayon ^ 3;
     drone.masse_volumique = drone.masse / drone.volume;
-    z_cdm = pi * drone.masse_volumique * drone.rayon ^ 4 / (8*drone.masse);
+    z_cdm = pi * drone.masse_volumique * drone.rayon ^ 4 / (4 * drone.masse);
     drone.CDM = [0; 0; z_cdm];
 
     pcmOut = drone.CDM; % Ajuster avec le CDM des bras/moteurs et du Colis
