c362546a8f792c88ebbb48d8eb9385469960030c
Nicolas Depelteau <nicolas@depelteau.ca>
2022-10-03 14:04:07 -0400

    L en referentiel global

--- tp1/aa.m
+++ tp1/aa.m
@@ -4,7 +4,6 @@ function aaOut = aa(context)
     R_i = PositionMoteurMatrix(context);
     F_i = ForceMatrixForMotor(context);
     t_cm = MomentForceAuCentreDeMasse(R_i, F_i);
-    L = context.MI * context.va;
     va = context.va;
 
     % ajustement rotation
@@ -13,9 +12,9 @@ function aaOut = aa(context)
         0 1 0;
         -sin(theta) 0 cos(theta)];
     ajustement_rotationt_t_cm = R * t_cm.';
-    ajustement_rotationt_L = R * L;
     ajustement_rotationt_va = R * va;
+    L = context.MI * ajustement_rotationt_va;
 
     %Calcul de l'acceleration angulaire
-    aaOut = inv(context.MI) * (ajustement_rotationt_t_cm.' + cross(ajustement_rotationt_L.', ajustement_rotationt_va.')).';
+    aaOut = inv(context.MI) * (ajustement_rotationt_t_cm.' + cross(L.', ajustement_rotationt_va.')).';
 endfunction
