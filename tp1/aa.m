a829747f709fd2edef2d97155dfcf29975a9b541
Nicolas Depelteau <nicolas@depelteau.ca>
2022-10-03 11:25:56 -0400

    rename to LocalToGobalReferentielRotation

--- tp1/aa.m
+++ tp1/aa.m
@@ -2,9 +2,9 @@ function aaOut = aa(context)
 
     R_i = PositionMoteurMatrix(context);
     F_i = ForceMatrixForMotor(context);
-    t_cm = LocalToGobalReferentiel(context, MomentForceAuCentreDeMasse(R_i, F_i));
-    L = LocalToGobalReferentiel(context, context.MI * context.va);
-    va = LocalToGobalReferentiel(context, context.va);
+    t_cm = LocalToGobalReferentielRotation(context, MomentForceAuCentreDeMasse(R_i, F_i));
+    L = LocalToGobalReferentielRotation(context, context.MI * context.va);
+    va = LocalToGobalReferentielRotation(context, context.va);
 
     aaOut = inv(context.MI) * (t_cm + cross(L, va));
 endfunction
