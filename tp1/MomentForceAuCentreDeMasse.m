b9693a78aa9c57bf95ff809908fcb7cfc2794119
Nicolas Depelteau <nicolas@depelteau.ca>
2022-10-03 13:47:51 -0400

    fix cross product that use row vector instead of column vector

--- tp1/MomentForceAuCentreDeMasse.m
+++ tp1/MomentForceAuCentreDeMasse.m
@@ -2,6 +2,6 @@ function t_cm = MomentForceAuCentreDeMasse(R_i, F_i)
     %Calcul du moment de force au centre de masse
     t_cm = zeros(1,3);
     for i = 1:size(R_i,2)
-        t_cm = t_cm + cross(R_i(:,i), F_i(:,i));
+        t_cm = t_cm + cross(R_i(:,i).', F_i(:,i).');
     endfor
 endfunction
--- tp1/aa.m
+++ tp1/aa.m
@@ -12,10 +12,10 @@ function aaOut = aa(context)
     R = [cos(theta) 0 sin(theta);
         0 1 0;
         -sin(theta) 0 cos(theta)];
-    ajustement_rotationt_t_cm = R * t_cm;
+    ajustement_rotationt_t_cm = R * t_cm.';
     ajustement_rotationt_L = R * L;
     ajustement_rotationt_va = R * va;
 
     %Calcul de l'acceleration angulaire
-    aaOut = inv(context.MI) * (ajustement_rotationt_t_cm + cross(ajustement_rotationt_L, ajustement_rotationt_va));
+    aaOut = inv(context.MI) * (ajustement_rotationt_t_cm.' + cross(ajustement_rotationt_L.', ajustement_rotationt_va.')).';
 endfunction
