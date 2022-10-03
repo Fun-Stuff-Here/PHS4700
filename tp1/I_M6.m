eb7518f7a334fd5ce1e5d55afacf8593dedd4793
nourabi <n.abiad97@gmail.com>
2022-10-02 20:24:17 -0400

    Matrice inertie

---
+++ tp1/I_M6.m
@@ -0,0 +1,16 @@
+function I_M6= I_M6(context)
+   %moment d'inertie par rapport aux axes
+   I_zz = (context.m_m  /2)*(context.R_m)^2
+   I_xx_yy = (context.m_m  /4)*(context.R_m)^2 + (context.m_m  /12)*(context.H_m)^2
+
+    Ic = diag([I_xx_yy;I_xx_yy;I_zz])   %tensor
+
+    cdm_cylindre_x = (context.L_b + context.R_s+context.R_m)*cos(pi/3)
+    cdm_cylindre_y= -(context.L_b + context.R_s+context.R_m)*sin(pi/3)
+    cdm_cylindre_z= context.H_m / 2
+    cdm_cylindre = [cdm_cylindre_x;cdm_cylindre_y;cdm_cylindre_z]
+    dc = pcm(context) - cdm_cylindre
+    Id = id_matrix(dc);%translation des axes
+    I_M6= Ic + context.m_m * Id
+
+  endfunction
