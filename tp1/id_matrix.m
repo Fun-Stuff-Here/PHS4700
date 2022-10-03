ad8ba85f671a4eeec96b87504d40e09f8742b63a
nourabi <n.abiad97@gmail.com>
2022-10-03 13:43:07 -0400

    resolving merge conflicts

--- tp1/id_matrix.m
+++ tp1/id_matrix.m
@@ -5,5 +5,4 @@ function Id_matrix= id_matrix(dc_matrix) % matrice pour translation des axes par
       -(dc_matrix(3))*(dc_matrix(1)),-(dc_matrix(3))*(dc_matrix(2)),(dc_matrix(1))^2 + (dc_matrix(2))^2];
         Id_matrix=matrix;
 
-      Id_matrix=matrix;
   endfunction
