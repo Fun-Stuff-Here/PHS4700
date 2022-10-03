e5dad4bba60eb1504b7d06d490a38881823b66e6
unknown <george.w.salib@gmail.com>
2022-10-02 20:25:33 -0400

    ajout de cas 2

--- tp1/acm.m
+++ tp1/acm.m
@@ -9,6 +9,10 @@ function acmOut = acm(context)
   masse_colis = 0.8;
   acceleration_gravitationnelle = [0; 0; -9.81];
 
+  % Calculs pour aller des radians à des degrés
+  angle_rotation = (context.ar * 180) / pi;
+  matrice_rotation = roty(angle_rotation);
+
   % Calculs pour trouver les masses
   masse_totale_bras = masse_bras * nombre_bras;
   masse_totale_moteurs = masse_moteur * nombre_bras;
@@ -17,27 +21,49 @@ function acmOut = acm(context)
                      + masse_totale_moteurs;
   masse_totale = masse_colis + masse_totale_drone;
 
-  % Calculs pour trouver les forces gravitationnelles
-  force_gravitationnelle_drone = masse_totale_drone * acceleration_gravitationnelle;
-  force_gravitationnelle_colis = masse_colis * acceleration_gravitationnelle;
+  % Calculs pour trouver la force gravitationnelle
+  force_gravitationnelle_totale = masse_totale * acceleration_gravitationnelle;
 
   % Calculs pour trouver les forces des moteurs
-  force_moteur1 = calculForceMoteur(context.Lambda(1));
-  force_moteur2 = calculForceMoteur(context.Lambda(2));
-  force_moteur3 = calculForceMoteur(context.Lambda(3));
-  force_moteur4 = calculForceMoteur(context.Lambda(4));
-  force_moteur5 = calculForceMoteur(context.Lambda(5));
-  force_moteur6 = calculForceMoteur(context.Lambda(6));
-  force_resultante = force_moteur1
-                   + force_moteur2
-                   + force_moteur3
-                   + force_moteur4
-                   + force_moteur5
-                   + force_moteur6
-                   + force_gravitationnelle_colis
-                   + force_gravitationnelle_drone;
+  force_totale_moteurs = context.Lambda * context.F_max;
+  force_moteur1 = [0; 0; force_totale_moteurs(1)];
+  force_moteur2 = [0; 0; force_totale_moteurs(2)];
+  force_moteur3 = [0; 0; force_totale_moteurs(3)];
+  force_moteur4 = [0; 0; force_totale_moteurs(4)];
+  force_moteur5 = [0; 0; force_totale_moteurs(5)];
+  force_moteur6 = [0; 0; force_totale_moteurs(6)];
+
+  % Calculs pour appliquer la rotation sur les forces des moteurs
+  force_moteur1_rotation = matrice_rotation * force_moteur1;
+  force_moteur2_rotation = matrice_rotation * force_moteur2;
+  force_moteur3_rotation = matrice_rotation * force_moteur3;
+  force_moteur4_rotation = matrice_rotation * force_moteur4;
+  force_moteur5_rotation = matrice_rotation * force_moteur5;
+  force_moteur6_rotation = matrice_rotation * force_moteur6;
 
   % Calcul pour trouver l'accélération du centre de masse
-  acmOut = force_resultante / masse_totale;
+  acmOut_X = force_moteur1_rotation(1) 
+            + force_moteur2_rotation(1)
+            + force_moteur3_rotation(1)
+            + force_moteur4_rotation(1)
+            + force_moteur5_rotation(1) 
+            + force_moteur6_rotation(1);
+
+  acmOut_Y = force_moteur1_rotation(2) 
+            + force_moteur2_rotation(2) 
+            + force_moteur3_rotation(2) 
+            + force_moteur4_rotation(2) 
+            + force_moteur5_rotation(2)
+            + force_moteur6_rotation(2);
+
+  acmOut_Z = force_moteur1_rotation(3) 
+            + force_moteur2_rotation(3)
+            + force_moteur3_rotation(3) 
+            + force_moteur4_rotation(3) 
+            + force_moteur5_rotation(3)
+            + force_moteur6_rotation(3) 
+            + force_gravitationnelle_totale;
+
+  acmOut = [acmOut_X; acmOut_Y; acmOut_Z] / masse_totale;
 
 endfunction
