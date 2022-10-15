1d0e6e31c790f04bfeb228fe31689e885126d831
Maxime Laroche <mlaroche2009@gmail.com>
2022-10-15 13:28:53 -0400

    implemented coefficient of drag function

--- tp2/rho.m
+++ tp2/rho.m
@@ -1,4 +1,8 @@
 function p = rho(position_fusee)
     % position_fusee: la position de la fusée [x;y;z]
-    p = 0;
+    rho_o = 1.275; % Kg/m^3
+    H_0 = 7200; % mètres
+    R_t = 6371 * (10 ^ 3); % En mètres
+    norme_position = norm(position_fusee);
+    p = rho_o * exp( (R_t - norme_position)/H_0 );
 endfunction
