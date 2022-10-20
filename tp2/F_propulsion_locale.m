934e971160dc98ddeceecac33392885e0e992ee5
Maxime Laroche <mlaroche2009@gmail.com>
2022-10-19 20:37:56 -0400

    Fait la force de propulsion

--- tp2/F_propulsion_locale.m
+++ tp2/F_propulsion_locale.m
@@ -1,4 +1,18 @@
 function f = F_propulsion_locale(context)
-    % vitesse [Vx;Vy;Vz] m/s. Utilisé pour avoir la 
-    f = [0;0;0];
+    % context de la forme {theta: float, mu: float}
+    % Theta: angle en radian du propulseur
+    % mu: débit massique de l'éjection des gaz. Dans le De2, c'est 1200 ou 0 (s'il n'y a plus de gaz)
+
+    context = {};
+    context.theta = 0.2;
+    context.mu = 1200;
+
+    v_gaz = [0 sin(context.theta) cos(context.theta)]; % en m/s
+    f = - context.mu * v_gaz;
 endfunction
+
+context = {};
+context.mu = 1200;
+context.theta = 0.2;
+x = F_propulsion_locale(context);
+fprintf(x);
