9ea12f432e7967138db07cb3afa223077debc4e6
Maxime Laroche <mlaroche2009@gmail.com>
2022-10-19 20:30:56 -0400

    Ajouté force gravitationelle

--- tp2/F_gravite_globale.m
+++ tp2/F_gravite_globale.m
@@ -1,3 +1,11 @@
 function f = F_gravite_globale(context)
-    f = [0;0;0];
+    masse_terre = 3000000000000; % kg
+    masse_fusee = 2; % kg
+    G = 6.673 * 10 ^(-11);  % Nm²/Kg²
+    r_fusee = [2 3 4];
+    option_euclidienne = 2;
+    f = (-G * masse_fusee * masse_terre / ( norm(r_fusee, option_euclidienne) ^3 )) * r_fusee;
 endfunction
+
+x= F_gravite_globale({});
+fprintf(x);
