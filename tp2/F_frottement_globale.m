1c5e9cdf948355242ec9e099968ad697ed0cbb13
Maxime Laroche <mlaroche2009@gmail.com>
2022-10-23 11:16:30 -0400

    ajouter calcul de alpha dans frottement

--- tp2/F_frottement_globale.m
+++ tp2/F_frottement_globale.m
@@ -5,7 +5,9 @@ function f = F_frottement_globale(context)
     C_vis = context.C_vis;
     vitesse_fusee = context.v_fusee;
     p = rho(position_fusee);
-    alpha = 1;
+
+    vz = context.v_fusee(3);
+    alpha = acos(vz/norm(context.v_fusee));
     A = pi * R^2  * cos(alpha) + 2 * R * H * sin(alpha);
 
     f = -1/2 * A * p * C_vis * norm(vitesse_fusee) * vitesse_fusee;
