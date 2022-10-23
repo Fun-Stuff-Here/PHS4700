b2e53e54e68378268e82e078d05eb2b834c04fa3
Maxime Laroche <mlaroche2009@gmail.com>
2022-10-23 10:37:22 -0400

    Fait force de propulsion globale

--- tp2/F_propulsion_globale.m
+++ tp2/F_propulsion_globale.m
@@ -1,4 +1,8 @@
 function f = F_propulsion_globale(context)
     % vitesse [Vx;Vy;Vz] m/s. Utilisé pour avoir la 
-    f = [0;0;0];
+    context = {};
+    f_locale = F_propulsion_locale(context);
+    q_f_locale = [0 f_locale];
+    q_rotation = [0 1 2 3];
+    f = QRotation(q_rotation, q_f_locale)
 endfunction
