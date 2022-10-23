5d978d5d10df216588d8f9fa90a5efaea90edaa4
Maxime Laroche <mlaroche2009@gmail.com>
2022-10-23 10:23:37 -0400

    force de frottement

--- tp2/F_frottement_globale.m
+++ tp2/F_frottement_globale.m
@@ -1,3 +1,12 @@
 function f = F_frottement_globale(context)
-    f = [0;0;0];
+    position_fusee = (6371*(10^3) + 0.1 * (10^3)) * [0 1 0]; % 100m d'altitude en y
+    R = 10;
+    H = 50;
+    C_vis = 0.5;
+    vitesse_fusee = [4 5 6];
+    p = rho(position_fusee);
+    alpha = 1
+    A = pi * R^2  * cos(alpha) + 2 * R * H * sin(alpha);
+
+    f = -1/2 * A * p * C_vis * norm(vitesse_fusee) * vitesse_fusee;
 endfunction
--- tp2/rho.m
+++ tp2/rho.m
@@ -1,5 +1,5 @@
 function p = rho(position_fusee)
-    % position_fusee: la position de la fusée [x;y;z]
+    % position_fusee: la position de la fusée [x y z]
     rho_o = 1.275; % Kg/m^3
     H_0 = 7200; % mètres
     R_t = 6371 * (10 ^ 3); % En mètres
