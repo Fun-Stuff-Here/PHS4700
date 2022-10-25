f5399081a4b58dd4cdc9fdb088d4a3ab792ddbe3
Nicolas Depelteau <nicolas@depelteau.ca>
2022-10-25 17:18:17 -0400

    Revert "update masse "
    This reverts commit ecb94eb014d850eff666101c8d2a7ce936cd5668.

--- tp2/g.m
+++ tp2/g.m
@@ -4,11 +4,11 @@ function [delta_q] =  g(q, t)
     terre.MASSE = 5.974 * (10 ^ 24); % En Kg
 
     fusee = {};
-    fusee.MASSE_INITIALE = 620 * (10 ^ 3); % En Kg
+    fusee.MASSE_INITIALE = 320 * (10 ^ 3); % En Kg
     fusee.HAUTEUR = 53; % En mètres
     fusee.RAYON = 1.8; % En mètres
     fusee.V_GAZ = 3.5 * (10 ^ 3); % En mètres / secondes
-    fusee.MASSE_CARBURANT_INITIAL = 300 * (10 ^ 3); % En kg
+    fusee.MASSE_CARBURANT_INITIAL = 270 * (10 ^ 3); % En kg
     fusee.POS_PROPULSEUR = [0; 0; -25]; % [x;y;z] en mètres par rapport au CDM de la fusée
     fusee.DEBIT_MASSIQUE_GAZ = 1200; % Kg/s
     fusee.COEFF_TRAINEE = 0.5; % Pas d'unité à ce que je sache;
